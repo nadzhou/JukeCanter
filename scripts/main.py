c243d3e31e430e65e13b13247c15e1b4616bf1c1
nadzhou <ahmaddnavid@gmail.com>
2021-10-02 16:09:27 +0500

    Added count plots.

--- scripts/main.py
+++ scripts/main.py
@@ -3,22 +3,37 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def main():
-    data = SeqIO.parse("scripts/data/covid/data.fasta", "fasta")
+import numpy as np
+
+
+def read_seq(file):
+    return SeqIO.parse(file, "fasta")
+
+
+def get_dna_counts(data):
     seqs = []
-    for sq in data:
-        seqs.append(str(sq.seq))
-    count = Counter(str(seqs))
+    for idx, seq in enumerate(data):
+        if idx < 2:
+            seqs.append(seq.seq)
 
-    dna = ["A", "T", "C", "G"]
-    dna_counts = {}
-    for key, val in count.items():
-        if key in dna:
-            dna_counts[key] = val
-    print(dna_counts)
-    plt.bar(dna_counts.keys(), dna_counts.values())
+    for sq in seqs:
+        count = Counter(sq)
+        dna = ["A", "T", "C", "G"]
+        dna_counts = {}
+        for key, val in count.items():
+            if key in dna:
+                dna_counts[key] = val
+        print(dna_counts)
+        plt.bar(dna_counts.keys(), dna_counts.values())
     plt.show()
+    return dna_counts
 
+
+
+def main():
+    file = "scripts/data/covid/data.fasta"
+    data = read_seq(file)
+    get_dna_counts(data)
 
 
 if __name__ == "__main__":
