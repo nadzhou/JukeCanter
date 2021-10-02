db52ff504c085d3aa5280d4180897ce25af2729b
nadzhou <ahmaddnavid@gmail.com>
2021-10-02 15:38:36 +0500

    Added plotting to count dna sequences from a file.

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,23 @@
 from Bio import SeqIO
-
+from collections import Counter
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def main():
+    data = SeqIO.parse("scripts/data/covid/data.fasta", "fasta")
+    seqs = []
+    for sq in data:
+        seqs.append(str(sq.seq))
+    count = Counter(str(seqs))
+
+    dna = ["A", "T", "C", "G"]
+    dna_counts = {}
+    for key, val in count.items():
+        if key in dna:
+            dna_counts[key] = val
+    print(dna_counts)
+    plt.bar(dna_counts.keys(), dna_counts.values())
+    plt.show()
 
 
 
